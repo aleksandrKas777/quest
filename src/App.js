dd655060b99470108f03dbc5c0fb7aaf26eb21ff
aleksandr <kasaleksandr@gmail.com>
2022-05-01 23:04:39 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -6,25 +6,68 @@ import govorun from "./images/govorun.jpg"
 import brest from "./images/Brest.jpg"
 import art from './images/aqua.jpg'
 import maps from './images/maps.jpg'
+import tort from './images/tort.png'
+
+const arrText = {
+    0: {
+        task: 'в первом задании тебе нужно ввести номер по которому можно позвонить тебе домой с домофона)',
+        modalText: 'Верно!!!'
+    },
+    1: {
+        task: 'А теперь введи код от домофона твой предыдущей квартиры=)',
+        modalText: 'На самом деле мы не знали этот код, поэтому тут подошло бы любое значение=))))'
+    },
+    2: {
+        task: 'Этот дом ты должна знать. В ответ нужно записать слово закрашенное красным цветом.',
+        modalText: 'ты справилась с этим заданием!!!'
+    },
+    3: {
+        task: 'теперь встань на то место, где было сделано это фото и нажми Ок)))',
+        modalText: 'молодец!!!'
+    },
+    4: {
+        task: 'Обернись, за твоей спиной баннер. Иди по направлению стрелки к серому ящику. Переходи дорогу только на зеленый!!! Напиши в ответ номер шкафа)',
+        modalText: 'СУПЕР!!!'
+    },
+    5: {
+        task: "Садись на 116 автобус и отправляйся на Кунцевщину) Там есть необычный термометр, судя по надписи возле него, он не должен измерять температуру в цельсиях... Запиши в ответ значение при котором этот термометр перестанем показывать правильную температуру",
+        modalText: 'Огонь!!!'
+    },
+    6: {
+        task: "В древности верили, что Землю держат 3 слона, которые стоят на черепахе. Здесь на рынке на черепахе стоит кто-то другой... Кто? )))",
+        modalText: "Вау, круто!!!"
+    },
+    7: {
+        task: "Между Кунцевщиной и Спортивной видели сахатых, тебе нужно найти это место и хорошенько там осмотреться. В ответ нужно записать номер телефона, который там найдешь))",
+        modalText: "На мозгобойне с тобой всех порвем в следующий раз"
+    },
+    8: {
+        task: "Теперь двигаясь к м.Спортивная нужно как-то найти передвижную бургерную и в ответ записать ее регистрационный знак",
+        modalText: "Молодец!!!"
+    },
+    9: {
+        task: "найди место на фото) примерно где-то здесь на карте)напиши в ответ название автора этого рисунка",
+        modalText: "ты почти у цели!!!!"
+
+    },
+    10: {
+        task: "Открой этот тайник и нажми Ок)",
+        modalText: "Cупер!!! Приз твой)))"
+    }
+
+}
 
 function App() {
-    const [stage, setStage] = useState( Number(localStorage.getItem('questJulia')) || 0)
+    const [stage, setStage] = useState(Number(localStorage.getItem('questJulia')) || 0)
     const [valueInput, setValueInput] = useState('')
     const arrCorrectAnswers = ['170b', 'all', 'говорун', 'all', '334', '37', 'лев', '397-50-10', 'a1161b-2', 'art&shock', 'all']
     const [count, setCount] = useState(0)
     const [showModal, setShowModal] = useState(false)
     const [textError, setTextError] = useState('')
-
-    // useEffect(() => {
-    //     setStage(Number(localStorage.getItem('todoList')) || 0)
-    // }, [])
+//  "homepage": "/quest/build",
     useEffect(() => {
-
-            localStorage.setItem('questJulia', String(stage));
-
-
-    },[stage])
-
+        localStorage.setItem('questJulia', String(stage));
+    }, [stage])
 
 
     const checkResult = () => {
@@ -77,54 +120,6 @@ function App() {
         }
 
     }
-    const arrText = {
-        0: {
-            task: 'в первом задании тебе нужно ввести номер по которому можно позвонить тебе домой с домофона)',
-            modalText: 'Верно!!!'
-        },
-        1: {
-            task: 'А теперь введи код от домофона твой предыдущей квартиры=)',
-            modalText: 'На самом деле мы не знали этот код, поэтому тут подошло бы любое значение=))))'
-        },
-        2: {
-            task: 'Этот дом ты должна знать. В ответ нужно записать слово закрашенное красным цветом.',
-            modalText: 'ты справилась с этим заданием!!!'
-        },
-        3: {
-            task: 'теперь встань на то место, где было сделано это фото и нажми Ок)))',
-            modalText: 'молодец!!!'
-        },
-        4: {
-            task: 'Обернись, за твоей спиной баннер. Иди по направлению стрелки к серому ящику. Переходи дорогу только на зеленый!!! Напиши в ответ номер шкафа)',
-            modalText: 'СУПЕР!!!'
-        },
-        5: {
-            task: "Садись на 116 автобус и отправляйся на Кунцевщину) Там есть необычный термометр, судя по надписи возле него, он не должен измерять температуру в цельсиях... Запиши в ответ значение при котором этот термометр перестанем показывать правильную температуру",
-            modalText: 'Огонь!!!'
-        },
-        6: {
-            task: "В древности верили, что Землю держат 3 слона, которые стоят на черепахе. Здесь на рынке на черепахе стоит кто-то другой... Кто? )))",
-            modalText: "Вау, круто!!!"
-        },
-        7: {
-            task: "Между Кунцевщиной и Спортивной видели сахатых, тебе нужно найти это место и хорошенько там осмотреться. В ответ нужно записать номер телефона, который там найдешь))",
-            modalText: "На мозгобойне с тобой всех порвем в следующий раз"
-        },
-        8: {
-            task: "Теперь двигаясь к м.Спортивная нужно как-то найти передвижную бургерную и в ответ записать ее регистрационный знак",
-            modalText: "Молодец!!!"
-        },
-        9: {
-            task: "найди место на фото) примерно где-то здесь на карте)напиши в ответ название автора этого рисунка",
-            modalText: "ты почти у цели!!!!"
-
-        },
-        10: {
-            task: "Открой этот тайник и нажми Ок)",
-            modalText: "Cупер!!! Приз твой)))"
-        }
-
-    }
 
 
     return (
@@ -133,12 +128,14 @@ function App() {
                 <Modal setShowModal={setShowModal} textModal={textError ? textError : arrText[stage].modalText}/>
             }
             <header className={'mb-3'} style={{backgroundColor: '#7952b3', height: '40px', color: '#FFF'}}>
+                <img src={tort} alt={'tort'} style={{height: '92%'}}/>
                 <div>Happy Birthday</div>
             </header>
-            {stage < 99 && <main className={'p-2'}>
+            {stage < 99 && <main className={'p-2 task'}>
                 <div className={'mb-2'} style={{
-                    border: '2px solid #7952b3',
-                    borderRadius: '10px'
+                    border: '4px solid #7952b3',
+                    borderRadius: '10px',
+                    backgroundColor: '#FFF'
                 }}>{arrText[stage].task}</div>
 
                 {stage === 2 && <img src={govorun} style={{width: '100%'}}
@@ -156,13 +153,14 @@ function App() {
                     </div>}
 
 
-                <div className={''}>
-                    {`счетчик неправильных ответов: ${count}`}
+                <div className={'count'}>
+                    <span>{'счетчик неправильных ответов: '}</span><span className={'text-decoration-underline'}
+                                                                         style={{color: count < 1 ? '#17790c' : '#810505'}}>{count}</span>
                 </div>
                 <div className={'d-flex justify-content-center'}>
                     <input className={'form-control w-75 me-2'} value={valueInput}
                            onChange={(e) => setValueInput(e.target.value)}/>
-                    <button className={'btn btn-primary'} onClick={() => checkResult(valueInput)}>Ок</button>
+                    <button className={'btn btn-primary btn-text'} onClick={() => checkResult(valueInput)}>OK</button>
 
                 </div>
             </main>}
--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,10 +1,10 @@
-export const Modal = ({textModal, setShowModal}) => {
+export const Modal = ({textModal}) => {
 
     return (
-        <div className={'position-fixed vw-100 vh-100 d-flex justify-content-center align-items-start p-1'}
-             style={{backgroundColor: 'rgba(85,85,85,0.39)'}}>
+        <div className={'position-fixed vw-100 vh-100 d-flex justify-content-center align-items-start p-1 modal'}
+             style={{backgroundColor: 'rgba(85,85,85,0.59)'}}>
             <div style={{
-                border: '2px solid #7952b3',
+                border: '4px solid rgba(57,0,225,0.76)',
                 borderRadius: '10px',
                 padding: '30px',
                 backgroundColor: 'whitesmoke',
@@ -12,10 +12,8 @@ export const Modal = ({textModal, setShowModal}) => {
                 position: 'relative'
             }}>
 
-                <button className={'btn btn-close position-absolute top-0 end-0'} onClick={() => setShowModal(false)}/>
-
                 <div>{textModal}</div>
-
+                <div style={{color: 'rgba(57,0,225,0.76)'}}>123</div>
             </div>
 
         </div>
